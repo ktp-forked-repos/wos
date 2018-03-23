2af5bf305ee4673262163b3a1379774f67ef0382
ramboxeu <mail.rambox@gmail.com>
2018-03-23 17:27:29 +0100

    Add stuff

--- assets.js
+++ assets.js
@@ -1,6 +1,51 @@
 const Assets = {
     Laws: [
+	{
+		title:"prawo do sprawiedliwego procesu"
+	},
+	{
+		title:"prawo do wolności poglądów oraz do rozpowszechniania informacji"
+	},
+	{
+		title:"prawo do ochrony prawnej życia prywatnego"
+	},
+	{
+		title:"prawo do nienaruszalności mieszkania",
+	},
+	{
+		title:"wolność poruszania się po terytorium RP",
+	},
+	{
+		title:"wolność sumienia i religii",
+	},
+	{
+		title:"prawo rodziców do wychowania dzieci w zgodzie z własnymi przekonaniami",
+	},
+	{
+		title:"prawo do wolności oraz ochrony tajemnicy komunikowania się",
+	},
+	{
+		title:"prawo do życia",
+	},
+	{
+		title:"nietykalność i wolność osobista"
+	}
     ],
     Obligations: [
+	{
+		title:"obrona Ojczyzny",
+	},
+	{
+		title:"wierność i troska o wspólne dobro narodu i społeczeństwa",
+	},
+	{
+		title:"dbałość o stan naturalnego środowiska, a także ponoszenie odpowiedzialności za jego zagrożenie i niszczenie.",
+	},
+	{
+		title:"przestrzeganie prawa RP",
+	},
+	{
+		title:"ponoszenie ciąarów i świadczeń na rzecz państwa (np. płacenie podatków)",
+	}
     ]
 }
